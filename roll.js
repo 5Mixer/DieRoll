1988cb1051701ad71e276b6d270eb6c0a3ac17f7
Mixerman123 <dantheman41@live.com.au>
2016-01-24 14:40:58 +1100

    Made it use tokens, which means over 1 digit ints.

--- roll.js
+++ roll.js
@@ -1,7 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
 
-var helpText = "You need to pass in some roll info"
+var helpText = "You need to pass in some roll info. Examples... \n\nd -> Roll 1 die, 1-6\n3d -> Roll 3 die, 1-6\n3d9 -> Roll 3 die 1-9\nd3 -> Roll 1 die 1-3";
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
@@ -14,27 +14,43 @@ var server = http.createServer(function (request, response) {
 
 		var result = 1;
 
-		var tokens = "";
+		var tokenTypes = "";
+		var tokens = [];
+
 
 		for (i = 0; i < input.length; i++) {
 			if (isInt(ch(input,i+1))){
-				tokens+= "I"; //INT
+				tokenTypes+= "I"; //INT
+
+				var multiDigitInt = ch(input,i+1) + "";
+				while ( isInt(ch(input,i+2))){ //While the next char is an int... add it.
+					multiDigitInt += ch(input,i+2);
+					i++;
+				}
+				if (multiDigitInt > 99){
+					response.end("Bad exploiter, bad!");
+					return;
+				}
+				tokens.push(parseInt(multiDigitInt));
+
 			}else{
-				tokens+= "L"; //LETTER
+				tokenTypes+= "L"; //LETTER
+				tokens.push(ch(input,i+1));
 			}
 		}
+		console.log(tokenTypes);
 		console.log(tokens);
 
 
-		if ((ch(input,1) == "d" || ch(input,1) == "D") && tokens=="L" ){
+		if ((tokens[0] == "d" || tokens[0] == "D") && tokenTypes=="L" ){
 			result = randomInt (1,6);
 		}
 
-		if (tokens == "IL"){
+		if (tokenTypes == "IL"){
 			//first is int.
 			//Roll several die, 1-6
 			result = "";
-			var die = ch(input,1);
+			var die = tokens[0];
 			console.log(die);
 			for (i = 0; i < die; i++) {
 				result += randomInt(1,6);
@@ -44,17 +60,17 @@ var server = http.createServer(function (request, response) {
 			}
 		}
 
-		if (tokens == "LI"){
-			result = randomInt(1,parseInt(ch(input,2)));
+		if (tokenTypes == "LI"){
+			result = randomInt(1,tokens[1]);
 		}
-		if (tokens == "ILI"){
+		if (tokenTypes == "ILI"){
 			//first is int.
 			//Roll several die, 1-6
 			result = "";
-			var die = ch(input,1);
+			var die = tokens[0];
 			console.log(die);
 			for (i = 0; i < die; i++) {
-				result += randomInt(1,parseInt(ch(input,3)));
+				result += randomInt(1,parseInt(tokens[2]));
 				if (i != die-1){
 					result += ", ";
 				}
@@ -74,6 +90,7 @@ function randomInt (low, high) {
 function ch(str,place){
 	return str.charAt(place-1);
 }
+
 
 function isInt(chara){
 	var c =  chara;
